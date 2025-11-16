2fa8228c0930d68f34aa500088d59d724fbdf8d9
Gau_Candy <gaulollipop@gmail.com>
2025-11-16 10:08:20 +0700

    cập nhập cơ sở dữ liệu

--- database/migrations/001_add_pterodactyl_user_id.sql
+++
@@ -1,5 +0,0 @@
--- Add pterodactyl_user_id to users table
-ALTER TABLE users ADD COLUMN IF NOT EXISTS pterodactyl_user_id INTEGER;
-
--- Add index for faster lookups
-CREATE INDEX IF NOT EXISTS idx_users_pterodactyl_user_id ON users(pterodactyl_user_id);
